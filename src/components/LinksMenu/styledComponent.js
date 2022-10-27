1b6269443f89e36facc8fb864f513e137303f128
Nandakumar7328 <lachunandu333@gmail.com>
2022-10-27 11:44:57 +0000

    bug fixed code

---
+++ src/components/LinksMenu/styledComponent.js
@@ -0,0 +1,34 @@
+import {Link} from 'react-router-dom'
+import styled from 'styled-components'
+
+export default null
+
+export const LinkContainer = styled.li`
+  width: 100%;
+  height: 45px;
+  background-color: ${props => props.bgColor};
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  @media (max-width: 767px) {
+    min-width: 500px;
+  }
+`
+export const MenuLink = styled(Link)`
+  width: 100%;
+  padding: 10px 160px;
+  display: flex;
+  text-decoration: none;
+  @media (min-width: 768px) {
+    padding: 10px 40px;
+    justify-content: flex-start;
+  }
+`
+export const LinkName = styled.p`
+  font-size: 16px;
+  font-weight: bold;
+
+  font-family: 'Roboto';
+  margin-left: 22px;
+  color: ${props => props.color};
+`
